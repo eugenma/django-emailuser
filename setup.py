6c97ce8e8be17a36ba2c3c9d018ec15a05272152
EugenM <eugen.massini@gmail.com>
2021-04-10 00:01:06 +0200

    use setuptools_scm

--- setup.py
+++ setup.py
@@ -1,37 +1,3 @@
-import os
-from setuptools import find_packages, setup
+from setuptools import setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
-    README = readme.read()
-
-# allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
-
-setup(
-    name='django-emailuser',
-    version='0.1.0',
-    packages=find_packages(),
-    include_package_data=True,
-    license='BSD License',
-    description='A simple Django app with users identified by email instead of username.',
-    long_description=README,
-    url=None,
-    author='Eugen Massini',
-    author_email='eugen.massini@gmail.com',
-    classifiers=[
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Framework :: Django :: 1.8',
-        'Framework :: Django :: 1.11',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-    ],
-)
+setup(use_scp_version=True)
